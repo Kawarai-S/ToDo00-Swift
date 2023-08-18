dc8361d5c14e3394d8726c919ecafbeaeae0ee7c
Kawarai-S <kayawakakururu@icloud.com>
2023-08-18 18:22:27 +0900

    Initial Commit

---
+++ Todo_00/Todo_00App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_00App.swift
+//  Todo_00
+//
+//  Created by 瓦井つばさ on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct Todo_00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
