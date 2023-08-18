8e4058775312f261972f7ad4a112b9dbfcfb98a3
Kawarai-S <kayawakakururu@icloud.com>
2023-08-18 19:29:42 +0900

    仮

--- Todo_00/ContentView.swift
+++ Todo_00/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var taskData = [(title:"ジョギングする", completed: false),
+                           (title:"お花に水をやる", completed: false),
+                           (title:"部屋の掃除をする", completed: false),
+                           (title:"本を読む", completed: false),
+                           (title:"トイレ掃除", completed: false)]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List(0..<taskData.count, id: \.self){ index in
+                Button {
+                    taskData[index].completed = !taskData[index].completed
+                } label: {
+                    HStack{
+                        if taskData[index].completed == true{
+                            Image(systemName:"checkmark.circle.fill")
+                        }else{
+                            Image(systemName: "circle")
+                        }
+                        Text(taskData[index].title)
+                    }
+                }
+                .foregroundColor(.black)
+            }
+            .navigationTitle("ToDoリスト")
         }
-        .padding()
     }
 }
 
